34fdf5e5e206cd6c90588197cc6915376962e056
4trastos <93524280+4trastos@users.noreply.github.com>
2024-10-20 08:39:51 +0200

    Add files via upload

---
+++ Conditionals/ex00/main.cpp
@@ -0,0 +1,34 @@
+/* la sentencia if
+
+    if (condición)
+    {
+        Instrucciones 1;
+    }
+    else
+    {
+        Instrucciones 2;
+    }
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    int data = 0;
+
+    cout << "Insert a number: "; cin >> number;
+
+    if (number == 5)
+    {
+        cout << "Perfect, it's number 5" << endl;
+    }
+    else
+    {
+        cout << "Error: The number is different than 5" << endl;
+    }
+
+    return (0);
+}
